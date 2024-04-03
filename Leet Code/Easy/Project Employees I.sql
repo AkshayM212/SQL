b9ef2a87ab9f35f7192337d43fa6d4588587fb1e
Akshay M <57315281+AkshayM212@users.noreply.github.com>
2024-04-03 12:58:16 +0530

    Create Project Employees I.sql

---
+++ Leet Code/Easy/Project Employees I.sql
@@ -0,0 +1,6 @@
+/*
+Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+*/
+SELECT P.project_id , ROUND(AVG(experience_years),2) AS average_years FROM Project P
+LEFT JOIN Employee E ON P.employee_id  = E.employee_id
+GROUP BY project_id
