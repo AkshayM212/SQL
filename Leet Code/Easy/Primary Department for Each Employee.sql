83a0f7ae6eb8b2601a1a02152f03f1996e60d6c5
Akshay M <57315281+AkshayM212@users.noreply.github.com>
2024-07-31 00:54:40 +0530

    Create Primary Department for Each Employee.sql

---
+++ Leet Code/Easy/Primary Department for Each Employee.sql
@@ -0,0 +1,20 @@
+/*
+
+Employees can belong to multiple departments. When the employee joins other departments, they need to decide which department is their primary department. Note that when an employee belongs to only one department, their primary column is 'N'.
+
+Write a solution to report all the employees with their primary department. For employees who belong to one department, report their only department.
+
+Return the result table in any order
+*/
+
+-- Write your PostgreSQL query statement below
+
+select employee_id, department_id
+from Employee
+where primary_flag = 'Y'
+or employee_id in (
+    select employee_id
+    from Employee
+    group by employee_id
+    having count(employee_id) = 1
+);
